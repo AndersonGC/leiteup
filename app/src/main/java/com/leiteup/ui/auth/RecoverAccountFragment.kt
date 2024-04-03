723f7fa7b5a8d20b9a204dce659edb29b2d29cc4
Anderson <andeersongc@gmail.com>
2024-04-03 02:54:53 -0300

    Adicionando autenticaÃção via firebase

--- app/src/main/java/com/leiteup/ui/auth/RecoverAccountFragment.kt
+++ app/src/main/java/com/leiteup/ui/auth/RecoverAccountFragment.kt
@@ -5,15 +5,72 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.leiteup.R
+import com.leiteup.databinding.FragmentLoginBinding
+import com.leiteup.databinding.FragmentRecoverAccountBinding
 
 class RecoverAccountFragment : Fragment() {
+
+    private var _binding: FragmentRecoverAccountBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recover_account, container, false)
+    ): View {
+
+        _binding = FragmentRecoverAccountBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+
+        initClicks()
+    }
+
+    private fun initClicks() {
+        binding.btnSend.setOnClickListener {
+            validateData()
+        }
+    }
+
+    private fun validateData() {
+        val email = binding.edtEmail.text.toString().trim()
+
+        if(email.isNotEmpty()) {
+            binding.progressBar.isVisible = true
+
+            recoverAccountUser(email)
+        } else {
+            Toast.makeText(requireContext(), "Informe seu e-mail.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun recoverAccountUser(email: String) {
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(requireContext(), "Foi enviado um e-mail para a redefinição de senha", Toast.LENGTH_SHORT).show()
+                } else {
+                    binding.progressBar.isVisible = false;
+                }
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
