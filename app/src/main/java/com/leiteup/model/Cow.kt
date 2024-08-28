5204d7962ba0b37ae1b1d0a415494054b05ba055
Anderson <andeersongc@gmail.com>
2024-08-28 01:48:59 -0300

    Criando a logica de ordenha e adicionando botão de exclusão

--- app/src/main/java/com/leiteup/model/Cow.kt
+++ app/src/main/java/com/leiteup/model/Cow.kt
@@ -4,14 +4,11 @@ import android.os.Parcelable
 import com.google.firebase.database.IgnoreExtraProperties
 import com.leiteup.helper.FirebaseHelper
 import kotlinx.parcelize.Parcelize
-import java.time.LocalDate
-import java.util.Date
 
 @Parcelize
 @IgnoreExtraProperties
 data class Cow(
 //    var cowImage: ByteArray? = null,
-//    var birthDay: LocalDate? = null,
     var breed: String = "",
     var earring: Int = 0,
     var father: String = "",
@@ -21,6 +18,7 @@ data class Cow(
     var mother: String = "",
     var name: String = "",
     var weight: Int = 0,
+    var birthDay: String = "",
 ) : Parcelable {
     init {
         this.id = FirebaseHelper.getDatabase().push().key ?: ""
