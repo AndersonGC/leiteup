643ce8cb01b0fe514080eec1b63a3eaf677f504f
Anderson <andeersongc@gmail.com>
2024-09-18 00:25:14 -0300

    Adicionando validacoes para evitar erros de cadastro

--- app/src/main/java/com/leiteup/ui/EditFormCowFragment.kt
+++ app/src/main/java/com/leiteup/ui/EditFormCowFragment.kt
@@ -24,6 +24,8 @@ class EditFormCowFragment : Fragment() {
 
     private lateinit var cow: Cow
 
+    private var isValid = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -72,13 +74,72 @@ class EditFormCowFragment : Fragment() {
 
     private fun initClicks() {
         view?.findViewById<Button>(R.id.btnAddCow)?.setOnClickListener {
-            validadeCow()
+            validadeEarring()
         }
     }
 
-    private fun validadeCow() {
-        val newEarring = binding.edtEarring.text.toString().trim().toInt()
-        val newName = binding.edtName.text.toString().trim().uppercase()
+    private fun validadeEarring() {
+        val newEarringText = binding.edtEarring.text.toString().trim()
+        val newCowName = binding.edtName.text.toString().uppercase()
+        if(newEarringText.isNotEmpty()) {
+            if(newEarringText.toInt() != cow.earring) {
+                cowController.cowExistsByEarring(newEarringText.toInt(), { exists ->
+                    if (exists) {
+                        binding.edtEarring.setBackgroundResource(R.drawable.bg_input_error)
+                        isValid = false
+                        Toast.makeText(requireContext(), "Brinco já cadastrado, insira um brinco válido.", Toast.LENGTH_SHORT).show()
+                        binding.root.scrollTo(0, 0)
+                    } else {
+                        isValid = true
+                        binding.edtEarring.setBackgroundResource(R.drawable.bg_input_text)
+                        validateCow(newEarringText, newCowName)
+                    }
+                }, { error ->
+                    Toast.makeText(requireContext(), "o.", Toast.LENGTH_SHORT).show()
+                })
+            } else {
+                isValid = true
+                binding.edtEarring.setBackgroundResource(R.drawable.bg_input_text)
+                validateCow(newEarringText, newCowName)
+            }
+        } else {
+            isValid = false
+            binding.edtEarring.setBackgroundResource(R.drawable.bg_input_error)
+            Toast.makeText(requireContext(), "O campo brinco não pode estar em branco.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun validateCow(newEarringText: String, newCowName: String) {
+        if(newCowName.isNotEmpty()) {
+            if(newCowName.uppercase() != cow.name) {
+                cowController.cowExists(newCowName, { exists ->
+                    if (exists) {
+                        binding.edtName.setBackgroundResource(R.drawable.bg_input_error)
+                        isValid = false
+                        Toast.makeText(requireContext(), "Animal já cadastrado, insira um animal válido.", Toast.LENGTH_SHORT).show()
+                        binding.root.scrollTo(0, 0)
+                    } else {
+                        isValid = true
+                        binding.edtName.setBackgroundResource(R.drawable.bg_input_text)
+                        proceedWithValidation(newEarringText, newCowName)
+                    }
+                }, { error ->
+                    Toast.makeText(requireContext(), "o.", Toast.LENGTH_SHORT).show()
+                })
+            } else {
+                isValid = true
+                binding.edtName.setBackgroundResource(R.drawable.bg_input_text)
+                proceedWithValidation(newEarringText, newCowName)
+            }
+        } else {
+            isValid = false
+            binding.edtName.setBackgroundResource(R.drawable.bg_input_error)
+            Toast.makeText(requireContext(), "O campo nome não pode estar em branco.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun proceedWithValidation(newEarringText: String, newCowName: String) {
+
         var newGender = when (binding.rGender.checkedRadioButtonId) {
             R.id.btnMale -> "Macho"
             R.id.btnFemale -> "Fêmea"
@@ -100,31 +161,39 @@ class EditFormCowFragment : Fragment() {
         var newMother = binding.edtMother.text.toString().trim()
         Log.e("COW_ID", "ID COW" + cow.id)
 
-        val updatedCow = Cow(
-            id = cow.id,
-            earring = newEarring,
-            name = newName,
-            gender = newGender,
-            breed = newBreed,
-            weight = newWeight,
-            birthDay = newBirthDayFormatter,
-            isIATF = newIatf,
-            father = newFather,
-            mother = newMother
-        )
 
-        cowController.updateCowAndMilkings(
-            oldCowName = cow.name,
-            updatedCow = updatedCow, // O objeto Cow atualizado
-            onSuccess = {
-            findNavController().previousBackStackEntry?.savedStateHandle?.set("updatedCow", updatedCow)
-            findNavController().popBackStack()
-            Toast.makeText(requireContext(), "Animal atualizado com sucesso.", Toast.LENGTH_SHORT).show()
-            },
-            onError = { errorMessage ->
-                Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
-            }
-        )
+        if (!isValid) {
+            // Toast.makeText(requireContext(), "Por favor, preencha todos os campos obrigatórios.", Toast.LENGTH_SHORT).show()
+            binding.root.scrollTo(0,0)
+            Log.i("COW_VALIDATE", "CHAMOU: ")
+            return
+        } else {
+            val updatedCow = Cow(
+                id = cow.id,
+                earring = newEarringText.toInt(),
+                name = newCowName,
+                gender = newGender,
+                breed = newBreed,
+                weight = newWeight,
+                birthDay = newBirthDayFormatter,
+                isIATF = newIatf,
+                father = newFather,
+                mother = newMother
+            )
+
+            cowController.updateCowAndMilkings(
+                oldCowName = cow.name,
+                updatedCow = updatedCow, // O objeto Cow atualizado
+                onSuccess = {
+                    findNavController().previousBackStackEntry?.savedStateHandle?.set("updatedCow", updatedCow)
+                    findNavController().popBackStack()
+                    Toast.makeText(requireContext(), "Animal atualizado com sucesso.", Toast.LENGTH_SHORT).show()
+                },
+                onError = { errorMessage ->
+                    Toast.makeText(requireContext(), errorMessage, Toast.LENGTH_SHORT).show()
+                }
+            )
+        }
     }
 
     override fun onDestroyView() {
